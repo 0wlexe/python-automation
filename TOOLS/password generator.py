5b676d4901f0f7d0d53e843b23b450956f9c80a9
Bea <30362709+j4nedoe@users.noreply.github.com>
2021-02-27 00:43:37 -0300

    Add files via upload

---
+++ TOOLS/password generator.py
@@ -0,0 +1,13 @@
+import random, string
+
+size = int(input('Digite o tamanho da senha que você deseja: ')) # tamanho da senha a ser gerada
+
+chars = string.ascii_letters  + string.digits + '!@#$%&8()-+,;:/?'
+# recebe a estrutura da senha a ser gerada
+
+rnd = random.SystemRandom() 
+# os.urandom - gera numeros aleatorios a partir de fontes fornecidas pelo sistema operacional
+
+print(''.join(rnd.choice(chars) for i in range(size)))
+# retorna uma lista com os caracteres randomicos gerado pelo chars com a quantidade de caracteres de size
+
