8840cd3af5bd9cc5ec5c691cdb79b85c5e7bcac7
Bea <30362709+j4nedoe@users.noreply.github.com>
2021-03-12 23:38:06 -0300

    Add files via upload

---
+++ TOOLS/folder hider.py
@@ -0,0 +1,14 @@
+# Ferramenta para aa ocultaçao de pastas 
+
+import ctypes
+
+pasta = input("Digite o caminho da pasta a ser ocultada (Ex: C:/pasta): ")
+
+atributo_ocultar = 0x02 #Atributo decimal que indica que o arquivo sera ocultado
+
+retorno = ctypes.windll.kernel32.SetFileAttributesW(pasta, atributo_ocultar)
+
+if retorno:
+    print("A pasta foi ocultado!")
+else:
+    print("A pasta não foi ocultado!")
